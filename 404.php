8f26b13ffbaab0e8524b6f035e4b65803f6fc5d3
MatiasRoy-Rodriguez <mroy@makingsense.com>
2021-09-21 11:58:20 -0300

    feature: add 404 template

---
+++ 404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="https://cdn.fromdoppler.com/doppler-ui-library/v3.102.0/css/styles.css">
+	<script type="text/javascript" src="https://cdn.fromdoppler.com/doppler-ui-library/v3.102.0/js/app.js"></script>
+	<link rel="stylesheet" href="https://use.typekit.net/fbq8dbp.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
+
+<body class="dp-library body-404">
+	<div class="dp-container-fluid">
+		<img src="img/logoEMMS.svg?version=<?= VERSION ?>" alt="Logo EMMS" id="logo-emms-404">
+	</div>
+	<div class="dp-container">
+
+		<div class="dp-rowflex">
+			<div class="error-404-container col-md-12">
+				<div class="error-404">
+				</div>
+			</div>
+			<div class="col-md-12 error-404-message">
+				<p>¿Perdido? Si quieres ver las mejores conferencias <br> online y gratis de Marketing...
+				</p>
+				<button type="button" class="dp-button" id="error-404-btn">
+					TOMA ESTE CAMINO
+				</button>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+
+</html>
