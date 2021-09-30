430d33359f02852e116261e7fd5d043f47bf7218
MS\hcardoso <hernan.f.cardoso@gmail.com>
2021-09-30 17:17:34 -0300

    refactoring tabla registrado. Se agrego el campo invito_dos_personas

--- services/insertarEnRegistrados.php
+++ services/insertarEnRegistrados.php
@@ -11,19 +11,20 @@ function saveRegistrado($usuario)
     $registrado = $db->query("SELECT id FROM registrados WHERE email='" . $usuario['email'] . "'");
     if ($registrado->fetchArray()) {
         //update
-        $fields = "SET nombre ='" . $usuario['nombre'] . "', apellido ='" . $usuario['apellido'] . "', register ='" . $usuario['register'] . "', ";
+        $fields = "SET invito_dos_personas ='" . $usuario['invito_dos_personas'] . "', nombre ='" . $usuario['nombre'] . "', apellido ='" . $usuario['apellido'] . "', register ='" . $usuario['register'] . "', ";
         $fields .= "pais ='" . $usuario['pais'] . "', telefono ='" . $usuario['telefono'] . "', empresa ='" . $usuario['empresa'] . "', ";
         $fields .= "source_utm ='" . $usuario['source_utm'] . "', medium_utm ='" . $usuario['medium_utm'] . "', campaign_utm ='" . $usuario['campaign_utm'] . "', ";
         $fields .= "content_utm ='" . $usuario['content_utm'] . "', term_utm ='" . $usuario['term_utm'] . "' ";
         $update = $db->query("UPDATE registrados $fields WHERE email='" . $usuario['email'] . "'");
         return $update;
     } else {
-        $fields = "(email, nombre, apellido, register, pais, telefono, empresa,";
+        $fields = "(email, nombre, apellido, invito_dos_personas, register, pais, telefono, empresa,";
         $fields .= "source_utm, medium_utm, campaign_utm, content_utm, term_utm)";
         $values = array(
             $usuario['email'],
             $usuario['nombre'],
             $usuario['apellido'],
+            $usuario['invito_dos_personas'],
             $usuario['register'],
             $usuario['pais'],
             $usuario['telefono'],
@@ -34,7 +35,7 @@ function saveRegistrado($usuario)
             $usuario['content_utm'],
             $usuario['term_utm']
         );
-        $insert = $db->query("INSERT INTO registrados $fields VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $values);
+        $insert = $db->query("INSERT INTO registrados $fields VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", $values);
         return $insert;
     }
 }
