81610569da7e4e66d9bafd463a7854e734b8011b
mcappato <mcappato@makingsense.com>
2021-09-30 13:46:06 -0300

    fix: refactor - extract code from main form states to separated pages states

---
+++ index-second-state.php
@@ -0,0 +1,113 @@
+	<div id="second-state">
+		<div class="dp-container-fluid" id="header">
+			<div class="dp-container">
+				<div class="dp-rowflex">
+					<div class="col-sm-12 col-md-6 col-lg-6">
+						<div class="col-sm-12 head-content-container">
+							<h1 id="title-second-state">¡Ya estás registrado! </h1>
+							<div class="head-content-text">
+								<p id="text-second-state">No te olvides de <a href="" target="_blank" rel="noopener">añadir este evento a
+										tu calendario</a> para no perderte nada del #EMMS2021. ¡Te esperamos el 11 de Noviembre! </p>
+								<p>Invita a dos colegas para que también puedan compartir este evento totalmente gratuito y online.</p>
+								<p id="timer-title">Faltan</p>
+								<div class="timer-container">
+									<div class="border"></div>
+									<div class="border"></div>
+									<div class="border"></div>
+									<div class="timer-data-container">
+										<span class="day" id="day"></span>
+										<span class="hour" id="hour"></span>
+										<span class="minute" id="minute"></span>
+										<p class="day-text">Días</p>
+										<p class="hour-text">Horas</p>
+										<p class="minute-text">Minutos</p>
+									</div>
+									<div id="emmsEnd"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-12 col-md-6 col-lg-6 register-form-container">
+						<div class="dp-rowflex">
+							<div class="col-md-2"></div>
+							<div class="col-sm-12 col-md-8 register-form">
+								<div class="dp-rowflex">
+									<div class="col-sm-0 col-md-1"></div>
+									<div class="col-sm-12 col-md-10">
+										<div class="dp-wrapper-form-plans">
+											<form class="dp-forms-plans" id="form" action="success-reservation.php" method="post">
+												<legend>Completa el siguiente formulario de EMMS</legend>
+												<ul class="field-group">
+													<li class="field-item">
+														<div class="holder">
+															<label for="email1">Email 01:</label>
+															<input type="email" name="email1" id="email1" placeholder="¡No olvides usar @!" class="email required">
+														</div>
+													</li>
+													<li class="field-item">
+														<div class="holder">
+															<label for="email2">Email 02:</label>
+															<input type="email" name="email2" id="email2" placeholder="¡No olvides usar @!" class="email required">
+														</div>
+													</li>
+												</ul>
+												<ul class="dp-exclusive-features-list field-group">
+													<li class="field-item field-item__checkbox">
+														<div class="holder">
+															<input name="Acepto la Política de Privacidad de Doppler" type="checkbox" id="acepto-politicas" value="true" class="required check"><span class="checkmark"></span><label for="acepto-politicas">
+																Acepto la Política de Privacidad de Doppler *
+															</label>
+														</div>
+													</li>
+													<li class="field-item field-item__checkbox">
+														<input name="Acepto recibir promociones de Doppler y sus aliados" type="checkbox" id="acepto-promociones" value="true"><span class="checkmark"></span><label for="acepto-promociones">
+															Acepto recibir promociones de Doppler y sus aliados</label>
+													</li>
+												</ul>
+
+												<div class="">
+													<div>
+														<!-- <input type="hidden" id="utm_source" name="utm_source" value='<?= $_GET['utm_source'] ?>' />
+														<input type="hidden" id="utm_campaign" name="utm_campaign" value='<?= $_GET['utm_campaign'] ?>' />
+														<input type="hidden" id="utm_content" name="utm_content" value='<?= $_GET['utm_content'] ?>' />
+														<input type="hidden" id="utm_term" name="utm_term" value='<?= $_GET['utm_term'] ?>' />
+														<input type="hidden" id="utm_medium" name="utm_medium" value='<?= $_GET['utm_medium'] ?>' />
+														<input type="hidden" id="origin" name="origin" value='<?= $_GET['origin'] ?>' /> -->
+														<button type="button" class="dp-button button-big" id="register-button-second-state">
+															ENVIAR
+														</button>
+													</div>
+													<div class="legal-form-container">
+														<p> Doppler te informa que los datos de carácter personal que nos proporciones al rellenar el
+															presente formulario serán tratados por Making Sense LLC (Doppler) como responsable de esta
+															web. <br>
+															<strong>Finalidad: </strong>Gestionar el alta de registro a la capacitación, enviarte
+															material
+															vinculado a la misma e información sobre Doppler y nuestros futuros eventos o
+															capacitaciones.
+															<br>
+															<strong>Legitimación: </strong>Consentimiento del interesado. <br>
+															<strong>Destinatarios: </strong>Tus datos serán guardados por Doppler, GoToWebinar como
+															plataforma de conferencias online y Digital Ocean como empresa de hosting. <br>
+															<strong>Información adicional: </strong>En la <a href="https://www.fromdoppler.com/es/legal/privacidad/" target="_blank" rel="noopener">Política de Privacidad</a> de Doppler encontrarás información adicional
+															sobre la recopilación y el uso de su información personal por parte de Doppler, incluida
+															información sobre acceso, conservación, rectificación, eliminación, seguridad,
+															transferencias
+															transfronterizas y otros temas. <br>
+														</p>
+													</div>
+												</div>
+
+
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
