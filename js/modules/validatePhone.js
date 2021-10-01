19eef2f04d1b466dfd978d20cc5063d0e9d86cf0
mcappato <mcappato@makingsense.com>
2021-10-01 08:56:34 -0300

    fix: update order, name files

---
+++ js/modules/validatePhone.js
@@ -0,0 +1,47 @@
+const setErrorField = (errorCode) => {
+
+    const errorMap = [
+        '¡Ouch! Escribe un teléfono válido.',
+        '¡Ouch! Escribe un código de area válido.',
+        '¡Ouch! El número de teléfono es demasiado corto.',
+        '¡Ouch! El número de teléfono es demasiado largo.',
+        '¡Ouch! Escribe un teléfono válido.',
+    ];
+
+    const holderPhone = document.getElementById("holder-phone");
+    holderPhone.setAttribute('data-error', errorMap[errorCode]);
+    holderPhone.classList.add("error");
+}
+const createPhoneField = () => {
+    const phoneInputField = document.getElementById("phone-input");
+
+    return window.intlTelInput(phoneInputField, {
+        utilsScript: "./js/modules/intlTelInput/util.js",
+        separateDialCode: true,
+        initialCountry: 'auto',
+        hiddenInput: 'full-phone-input',
+        geoIpLookup: function (callback) {
+            fetch('https://ipinfo.io')
+                .then(data => {
+                    const countryCode = data && data.country ? data.country : 'AR';
+                    callback(countryCode);
+                })
+        }
+    });
+}
+const validatePhoneField = (phoneInput) => {
+    const phoneInputField = document.getElementById("phone-input");
+    if (phoneInputField.value.trim()) {
+        if (!phoneInput.isValidNumber()) {
+            const errorCode = phoneInput.getValidationError();
+            setErrorField(errorCode);
+            return false;
+        }
+        return true;
+    }
+}
+
+export {
+    createPhoneField,
+    validatePhoneField
+};
