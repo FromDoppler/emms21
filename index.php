86a3f9a277d4b232d6e6e319b21c67a71ccaeddc
MatiasRoy-Rodriguez <mroy@makingsense.com>
2021-10-04 13:24:58 -0300

    feat: change speakers schedule

--- index.php
+++ index.php
@@ -62,8 +62,7 @@ require_once('./config.php');
 				</div>
 				<div class="col-md-2"></div>
 				<div class="col-md-8">
-					<p>Asiste al día y a la actividad que quieras. El Networking será exclusivo para usuarios de Doppler. ¿Aún no
-						tienes cuenta? <a href="#" target="_blank" rel="noopener">Crea una gratis.</a></p>
+					<p>Conoce a los notables speakers que nos acompañaran en esta jornada.</p>
 				</div>
 				<div class="col-md-2"></div>
 			</div>
@@ -224,41 +223,12 @@ require_once('./config.php');
 					</div>
 				</div>
 				<div class="col-sm-12 col-md-6 col-lg-4 speaker-col-center">
-					<div class="speaker-shadow">
-						<div class="speaker-container">
-							<div class="speaker">
-								<p class="name">Álvaro Gutiérrez de Cabiedes</p>
-								<p class="seniority">Sr. Manager de Transformación Digital</p>
-								<div class="redes">
-									<a href="" target="_blank" rel="nofollow"><img src="img/twitter.svg?version=<?= VERSION ?>" alt="Twitter" class="speaker-social-icon"></a>
-									<a href="" target="_blank" rel="nofollow"><img src="img/linkedin.svg?version=<?= VERSION ?>" alt="Linkedin" class="speaker-social-icon"></a>
-									<a href="" target="_blank" rel="nofollow"><img src="img/instagram.svg?version=<?= VERSION ?>" alt="Instagram" class="speaker-social-icon"></a>
-									<a href="" target="_blank" rel="nofollow"><img src="img/facebook.svg?version=<?= VERSION ?>" alt="Facebook" class="speaker-social-icon"></a>
-								</div>
-							</div>
-							<div class="picture" style="background-image: linear-gradient(90deg, rgba(0, 0, 0, 0.6) 0%, rgba(83, 83, 83, 0) 31.67%, rgba(255, 255, 255, 0) 100%), url(/img/speakerPic.png?version=<?= VERSION ?>);">
-							</div>
-						</div>
-						<div class="speaker-footer">
-							<p class="speaker-footer-info ">El impacto de la Transformación <br> Digital en la Banca</p>
-							<details class="details-animated">
-								<summary><img src="img/arrow.png?version=<?= VERSION ?>" alt="Flecha" class="speaker-arrow"></summary>
-								<div class="content">
-									<p>
-										El turismo tiene un fuerte componente intangible y emocional que puede ser un disparador de nuevos
-										modelos de negocio. Descubre las principales líneas de acción que llevarán tu idea al éxito.
-										Descubre las principales líneas de acción
-									</p>
-								</div>
-							</details>
-						</div>
-					</div>
+							<div class="speaker-bg-asset"></div>
 				</div>
 			</div>
 		</div>
 
 		<div class="dp-container-fluid" id="footer-index">
-			<div class="footer-bg-asset"></div>
 			<div class="dp-container footer-index-container">
 				<div class="dp-rowflex">
 					<div class="col-md-12">
