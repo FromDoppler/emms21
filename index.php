8b64d53ad9d9cc33c30ac0a1cdbc6189ce723ecc
MatiasRoy-Rodriguez <mroy@makingsense.com>
2021-10-04 14:49:36 -0300

    fix: change mobile footer

--- index.php
+++ index.php
@@ -246,8 +246,8 @@ require_once('./config.php');
 		<div class="dp-container-fluid" id="footer">
 			<div class="dp-rowflex">
 
-				<div class="col-sm-12 col-md-5">
-					<div class="footer-text">
+				<div class="col-sm-12 col-md-5 footer-text">
+					<div>
 						<p><strong>EMMS 2020</strong> - Un eventro creado por</p> <img src="img/LogoDoppler.png?version=<?= VERSION ?>" alt="Doppler">
 					</div>
 				</div>
@@ -258,6 +258,11 @@ require_once('./config.php');
 						<a href="#" target="_blank" rel="noopener"><img src="img/Youtube.svg?version=<?= VERSION ?>" alt="Youtube" class="img-social"></a>
 						<a href="#" target="_blank" rel="noopener"><img src="img/linkedin.svg?version=<?= VERSION ?>" alt="Linkeding" class="img-social"></a>
 						<a href="#" target="_blank" rel="noopener"><img src="img/twitter.svg?version=<?= VERSION ?>" alt="Twitter" class="img-social"></a>
+					</div>
+				</div>
+				<div class="col-sm-12 col-md-5 footer-text-mobile">
+					<div>
+						<p><strong>EMMS 2020</strong> - Un eventro creado por</p> <img src="img/LogoDoppler.png?version=<?= VERSION ?>" alt="Doppler">
 					</div>
 				</div>
 				<div class="col-sm-12 col-md-5">
