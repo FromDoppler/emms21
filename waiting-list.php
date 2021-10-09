7712a4d7b78ce13aa63cc16f9d0e458520c8aa17
MatiasRoy-Rodriguez <mroy@makingsense.com>
2021-10-09 11:24:34 -0300

    feat: DS-2813-add-waiting-template

---
+++ waiting-list.php
@@ -0,0 +1,21 @@
+<div id="waiting-state">
+	<div class="dp-container-fluid" id="header">
+		<div class="dp-container">
+			<div class="dp-rowflex">
+				<div class="col-sm-0 col-md-2 col-lg-2"></div>
+
+				<div class="col-sm-12 col-md-8 col-lg-8" id="inicio">
+					<div class="col-sm-12 head-content-container" id="head-content-container-waiting">
+						<h1 id="title-second-state">¡Pronto recibirás novedades!</h1>
+						<div class="head-content-text">
+							<p id="text-second-state">#EMMS2021 es un evento exclusivo con invitación, y hemos reservado un espacio en
+								nuestra lista de espera para ti. Pronto recibirás noticias en tu Casilla de Correo. </p>
+							<p><strong>¿Tienes una invitación? Ingresa desde el link que has recibido</strong></p>
+						</div>
+						<button>MIRA LAS EDICIONES ANTERIORES</button>
+					</div>
+				</div>
+				<div class="col-sm-0 col-md-2 col-lg-2"></div>
+			</div>
+		</div>
+	</div>
