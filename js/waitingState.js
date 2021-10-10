06acb2e0058b669dcb4b3be6badacdb468ca19ea
mcappato <mcappato@makingsense.com>
2021-10-10 16:19:38 -0300

    fix: waitingState

--- js/waitingState.js
+++ js/waitingState.js
@@ -3,26 +3,27 @@ import {
     showSecondState
 } from './statesHandler.js'
 
-const checkEndTime = (now) => {
+const checkEndTime = (refreshIntervalId,now) => {
     if (now > localStorage.et) {
         clearInterval(refreshIntervalId);
-        document.getElementById("title-second-state").innerHTML = "ya estas registrado!";
+        document.getElementById("title-waiting-state").innerHTML = "ya estas registrado!";
         localStorage.t="pr";
         showSecondState();
     }
 }
 
-const refreshIntervalId = setInterval( () => {
-    let now = new Date;
-    let distance = localStorage.et - now;
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("title-second-state").innerHTML = `En ${minutes}:${seconds} estaras registrado.`;
-    checkEndTime(now);
-},100);
-
+const startCountDown = () => {
+    const refreshIntervalId = setInterval( () => {
+        let now = new Date;
+        let distance = localStorage.et - now;
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById("title-waiting-state").innerHTML = `Estamos validando tus datos. <br> Tiempo de espera ${minutes.toString()}:${seconds.toString()} Segundos.`;
+        checkEndTime(refreshIntervalId,now);
+    },100);
+}
 export const waitingState = () => {
     const interval = 1000 * 60;
     localStorage.et = localStorage.et || +new Date + interval;
-    refreshIntervalId;
+    startCountDown();
 }
