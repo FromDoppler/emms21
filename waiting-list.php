7bd4fcbae0639ed163c3a3f6a74473021c4fe2bc
MatiasRoy-Rodriguez <mroy@makingsense.com>
2021-10-19 11:06:46 -0300

    fix: change visual styles

--- waiting-list.php
+++ waiting-list.php
@@ -8,7 +8,7 @@
 					<div class="col-sm-12 head-content-container" id="head-content-container-waiting">
 						<h1 id="title-waiting-state">¡Pronto recibirás novedades!</h1>
 						<div class="head-content-text">
-							<p id="text-waiting-state">EMMS 2021 es un evento exclusivo. es un evento exclusivo. ¡Hemos reservado un espacio en nuestra lista de espera para ti! Próximamente recibirás noticias en tu Casilla de Correo.</p>
+							<p id="text-waiting-state">EMMS 2021 es un evento exclusivo. ¡Hemos reservado un espacio en nuestra lista de espera para ti! <strong>Próximamente recibirás noticias en tu Casilla de Correo</strong>.</p>
 							<p><span>¿Ya tienes tu invitación?</span> Ingresa desde el link que has recibido</p>
 						</div>
 						<button onclick="window.location.href='ediciones-anteriores.php'">REVIVE EDICIONES ANTERIORES</button>
