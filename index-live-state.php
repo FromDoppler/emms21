4745818688677c5da5242865e2ff0a03f61d7ce6
MatiasRoy-Rodriguez <mroy@makingsense.com>
2021-11-01 15:36:41 -0300

    feat: responsive live state

--- index-live-state.php
+++ index-live-state.php
@@ -5,10 +5,10 @@
 				<div class="col-sm-12 col-md-12 col-lg-12">
 					<div class="live-container">
 						<span class="live-icon-container"><img src="app/1.0.0/img/liveIcon.png" alt="En vivo" class="live-icon"> EN VIVO </span>
-						<iframe width="100%" height="100%" src="https://www.youtube.com/embed/TYpRc1YIqtQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Nv9P2L85MIU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 					</div>
 					<div class="chat">
-						<img src="app/1.0.0/img/chat.png" alt="">
+						<iframe src="https://www.youtube.com/live_chat?v=Nv9P2L85MIU&embed_domain=localhost" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 					</div>
 				</div>
 			</div>
