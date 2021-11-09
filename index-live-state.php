8b0b20d329fa0eb8152c541944073569c84f048f
MS\hcardoso <hernan.f.cardoso@gmail.com>
2021-11-09 10:55:21 -0300

    muted autoplay for live video

--- index-live-state.php
+++ index-live-state.php
@@ -11,7 +11,7 @@ $settings = $db->query('SELECT * FROM settings ORDER BY id DESC LIMIT 1')->fetch
 				<div class="col-sm-12 col-md-12 col-lg-12">
 					<div class="live-container">
 						<span class="live-icon-container"><img src="app/<?= VERSION ?>/img/liveIcon.png" alt="En vivo" class="live-icon"> EN VIVO </span>
-						<iframe width="100%" height="100%" src="https://www.youtube.com/embed/<?= $settings['youtube'] ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						<iframe width="100%" height="100%" src="https://www.youtube.com/embed/<?= $settings['youtube'] ?>?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 					</div>
 					<div class="chat">
 						<iframe src="https://www.youtube.com/live_chat?v=<?= $settings['youtube'] ?>&embed_domain=<?= DOMAIN_IFRAME_CHAT ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
