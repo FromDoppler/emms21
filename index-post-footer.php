f982014eec06fb7398bf132e47ab3ec373fbd869
MatiasRoy-Rodriguez <mroy@makingsense.com>
2021-11-13 13:17:08 -0300

    feat: apply final content

--- index-post-footer.php
+++ index-post-footer.php
@@ -5,7 +5,7 @@
 				<div class="footer-post-container">
 					<h2>EVENTO CREADO POR <img src="img/logoDopplerPost.png" alt="Logo Doppler"></h2>
 					<p><i>¿Aún no tienes cuenta?</i> Conoce los beneficios del Email, Automation & Data Marketing</p>
-					<button>CREA TU CUENTA</button>
+					<button target="_blank" rel="noopener" onclick="window.location.href='https://app.fromdoppler.com/signup?lang=es&utm_medium=goemms&utm_source=EMMS2021&utm_campaign=post-emms'">CREA TU CUENTA</button>
 				</div>
 			</div>
 		</div>
