b72dbff8366370abdf04770b3fa44e19e042802c
mcappato <mcappato@makingsense.com>
2021-11-15 12:37:37 -0300

    fix: navigation bar speakers links

--- ricardo-diez.php
+++ ricardo-diez.php
@@ -71,13 +71,10 @@ require_once('./config.php');
 		<div class="dp-container-fluid" id="header">
 			<header class="header" id="navbar">
 				<nav class="navbar">
-					<a href="#inicio" class="nav-logo"><img src="img/logoEMMS.svg?version=<?= VERSION ?>" alt="Logo EMMS 2021" class="logoEMMS"></a>
+					<a href="/" class="nav-logo"><img src="img/logoEMMS.svg?version=<?= VERSION ?>" alt="Logo EMMS 2021" class="logoEMMS"></a>
 					<ul class="nav-menu">
 						<li class="nav-item">
-							<a href="#inicio" class="nav-link">Inicio</a>
-						</li>
-						<li class="nav-item">
-							<a href="#agenda" class="nav-link">Agenda</a>
+							<a href="/" class="nav-link">Inicio</a>
 						</li>
 						<li class="nav-item">
 							<a href="ediciones-anteriores.php#inicio" class="nav-link">Ediciones anteriores</a>
