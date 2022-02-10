7506d88040fa98be0248a0a264ea23b3f8e90dfd
Matías Roy Rodriguez <mroy@makingsense.com>
2022-02-10 13:47:29 -0300

    fix: add observer for php injection

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ require_once('./config.php');
 
 </head>
 
-<body class="dp-library">
+<body class="dp-library" id="body">
 	<!-- Google Tag Manager (noscript) -->
 	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M768WZR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	<!-- End Google Tag Manager (noscript) -->
