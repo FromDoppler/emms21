dd76298b4370cb2c83187efabe53770d2887834e
Matías Roy Rodriguez <mroy@makingsense.com>
2022-02-14 11:18:00 -0300

    fix: fix logic in utm

--- app/1.0.0/js/modules/utm/utm.js
+++ app/1.0.0/js/modules/utm/utm.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	let target = '';
 	// selecciona el nodo 
-	if (window.location.pathname.includes('.php')) {
+	if (window.location.pathname.includes('.php') && !(window.location.pathname.includes('index.php')) ) {
 		target = document.querySelector('#body');
 		getUtms();
 	} else {
