55cf9a344992f029787212c6961ce1a95cc26c5e
Matías Roy Rodriguez <mroy@makingsense.com>
2022-02-14 11:30:24 -0300

    feat: change popUp in desktop

--- popUp.php
+++ popUp.php
@@ -3,11 +3,9 @@
 		<button class="closePopUp">X</button>
 		<h2>¡Revive el EMMS con los líderes de la industria!</h2>
 		<p>Antes de continuar y ver los videos…¿Te gustaría comenzar a realizar Email, Automation & Data Marketing?</p>
-		<p class="dp-none-mobile last--p">Comienza hoy a enviar Campañas a tus clientes ¡Aprovecha una prueba gratuita por 90 días!</p>
-
 		<a id="ctaCreateAccount" href="https://app.fromdoppler.com/signup" target="_blank" rel="nofollow">Crea tu cuenta GRATIS</a>
 
-		<p  class="dp-on-mobile last--p closePopUp" >Accede a los videos y crea tu cuenta más tarde</p>
+		<p  class="last--p closePopUp" >Accede a los videos y crea tu cuenta más tarde</p>
 
 		<footer>© 2022. <a href="https://www.fromdoppler.com/es/legal/privacidad/" target="_blank" rel="nofollow">Doppler LLC</a>. Todos los derechos reservados.</footer>
 	</div>
